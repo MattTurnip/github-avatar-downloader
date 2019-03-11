591eddb8952e043838742afc36123059c5276f78
MattTurnip <mattadavey@gmail.com>
2019-03-11 23:30:50 +0000

    fixed a mistake where  were left off of authorization key ¯\_(ツ)_/¯

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,57 @@
+// var request = require("request");
+// var secrets = require("./secrets");
+// var fs = require("fs");
+
+// function getRepoContributors(repoOwner, repoName, cb) {
+//   var options = {
+//     url:"https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+//     headers: {
+//       "User-Agent": "request",
+//       Authorization: "token" + secrets.GITHUB_TOKEN
+//     }
+//   };
+
+//   request(options, function(err, res, body) {
+//     cb(err, body);
+//   });
+// }
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   if (!err) {
+//     var output = JSON.parse(result);
+//     for (var i = 0; i < output.length; i++) {
+//       downloadImageByURL(output[i].avatar_url, "avatars/" + output[i].login + ".jpg");
+//     }
+//   } else {
+//     throw err;
+//   }
+// });
+
+// function downloadImageByURL(url, filePath) {
+//   request
+//     .get(url + filePath)
+//     .on("error", function(err) {
+//       throw err;
+//     })
+//     .on("response", function(response) {
+//       return response.avatar_url;
+//     })
+//     .pipe(fs.createWriteStream(filePath));
+// }
+
 var request = require("request");
 var secrets = require("./secrets");
 var fs = require("fs");
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       "User-Agent": "request",
-      Authorization: "token" + secrets.GITHUB_TOKEN
+      "Authorization": "token " + secrets.GITHUB_TOKEN
     }
   };
-
   request(options, function (err, res, body) {
     cb(err, body);
   });
@@ -19,11 +60,9 @@ function getRepoContributors(repoOwner, repoName, cb) {
 getRepoContributors("jquery", "jquery", function (err, result) {
   if (!err) {
     var output = JSON.parse(result);
+
     for (var i = 0; i < output.length; i++) {
-      downloadImageByURL(
-        output[i].avatar_url,
-        "avatars/" + output[i].login + ".jpg"
-      );
+      downloadImageByURL(output[i].avatar_url, "avatars/" + output[i].login + ".jpg");
     }
   } else {
     throw err;
@@ -41,3 +80,4 @@ function downloadImageByURL(url, filePath) {
     })
     .pipe(fs.createWriteStream(filePath));
 }
+console.log("downloading avatars...")
