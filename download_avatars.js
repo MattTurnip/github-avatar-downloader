af695ac2e32e00106f0e1a52e72ebab9ca7957ca
MattTurnip <mattadavey@gmail.com>
2019-03-12 00:26:19 +0000

    removed erroneous comment filled with older version of code

--- download_avatars.js
+++ download_avatars.js
@@ -1,43 +1,4 @@
-// var request = require("request");
-// var secrets = require("./secrets");
-// var fs = require("fs");
 
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   var options = {
-//     url:"https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//     headers: {
-//       "User-Agent": "request",
-//       Authorization: "token" + secrets.GITHUB_TOKEN
-//     }
-//   };
-
-//   request(options, function(err, res, body) {
-//     cb(err, body);
-//   });
-// }
-
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   if (!err) {
-//     var output = JSON.parse(result);
-//     for (var i = 0; i < output.length; i++) {
-//       downloadImageByURL(output[i].avatar_url, "avatars/" + output[i].login + ".jpg");
-//     }
-//   } else {
-//     throw err;
-//   }
-// });
-
-// function downloadImageByURL(url, filePath) {
-//   request
-//     .get(url + filePath)
-//     .on("error", function(err) {
-//       throw err;
-//     })
-//     .on("response", function(response) {
-//       return response.avatar_url;
-//     })
-//     .pipe(fs.createWriteStream(filePath));
-// }
 
 require('dotenv').config();
 var request = require("request");
